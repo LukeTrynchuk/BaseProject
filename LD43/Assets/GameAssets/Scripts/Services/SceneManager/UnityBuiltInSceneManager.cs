d8fbb35f0daaf2b96c64032bce5f6ed3a9f235a3
Unknown <luke123t@gmail.com>
2018-11-28 01:25:33 -0400

    Add Scene Manager Service

---
+++ LD43/Assets/GameAssets/Scripts/Services/SceneManager/UnityBuiltInSceneManager.cs
@@ -0,0 +1,42 @@
+﻿using DogHouse.Core.Services;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DogHouse.Services
+{
+    /// <summary>
+    /// UnityBuiltInSceneManager will use the built
+    /// in Unity Scene Manager to load new scenes.
+    /// </summary>
+    public class UnityBuiltInSceneManager : MonoBehaviour, ISceneManager
+    {
+        #region Private Variables
+        private const string LOGO_SCENE = "LogoSlideShow";
+        #endregion
+
+        #region Main Methods
+        void OnEnable() => RegisterService();
+
+        void OnDisable() => UnregisterService();
+
+        public void LoadSlideShowScene() => Load(LOGO_SCENE);
+        
+        public void RegisterService()
+        {
+            ServiceLocator.Register<ISceneManager>(this);
+        }
+
+        public void UnregisterService()
+        {
+            ServiceLocator.Unregister<ISceneManager>(this);
+        }
+        #endregion
+
+        #region Utility Methods
+        private void Load(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        #endregion
+    }
+}
