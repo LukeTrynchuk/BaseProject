db016cbc5439310219b8e47c83e489df9c720124
Unknown <luke123t@gmail.com>
2018-11-28 22:12:50 -0400

    Add Transition from Slide Show to Main Menu

--- LD43/Assets/GameAssets/Scripts/Services/SceneManager/_Helpers/SceneManagerBackdoor.cs
+++ LD43/Assets/GameAssets/Scripts/Services/SceneManager/_Helpers/SceneManagerBackdoor.cs
@@ -1,26 +1,32 @@
 ﻿using DogHouse.Core.Services;
-using DogHouse.Services;
 using UnityEngine;
 
-namespace DogHouse.General
+namespace DogHouse.Services
 {
     /// <summary>
     /// CallLoadSlideShowScene is a component that can be
     /// used to easily access the SceneManager and call the
     /// Load Slide Show Scene method.
     /// </summary>
-    public class CallLoadSlideShowScene : MonoBehaviour 
+    public class SceneManagerBackdoor : MonoBehaviour , ISceneManager
     {
         #region Private Variables
         private ServiceReference<ISceneManager> m_sceneManager 
             = new ServiceReference<ISceneManager>();
         #endregion
-        
+
         #region Main Methods
         public void LoadSlideShowScene()
         {
             m_sceneManager.Reference?.LoadSlideShowScene();
         }
+
+        public void LoadMainMenuScene()
+        {
+            m_sceneManager.Reference?.LoadMainMenuScene();
+        }
+
+        public void RegisterService() {}                                        //Were not going to register this. This class is just a backdoor and thus not necesarry to register as a service.
         #endregion
     }
 }
