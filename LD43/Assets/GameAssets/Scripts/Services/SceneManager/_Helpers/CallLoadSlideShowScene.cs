abb7ce23b2f8fc4e8b5e51f685818f41dbd6abaf
Unknown <luke123t@gmail.com>
2018-11-28 21:53:15 -0400

    Add Preload to Slideshow Scene Transition

---
+++ LD43/Assets/GameAssets/Scripts/Services/SceneManager/_Helpers/CallLoadSlideShowScene.cs
@@ -0,0 +1,26 @@
+﻿using DogHouse.Core.Services;
+using DogHouse.Services;
+using UnityEngine;
+
+namespace DogHouse.General
+{
+    /// <summary>
+    /// CallLoadSlideShowScene is a component that can be
+    /// used to easily access the SceneManager and call the
+    /// Load Slide Show Scene method.
+    /// </summary>
+    public class CallLoadSlideShowScene : MonoBehaviour 
+    {
+        #region Private Variables
+        private ServiceReference<ISceneManager> m_sceneManager 
+            = new ServiceReference<ISceneManager>();
+        #endregion
+        
+        #region Main Methods
+        public void LoadSlideShowScene()
+        {
+            m_sceneManager.Reference?.LoadSlideShowScene();
+        }
+        #endregion
+    }
+}
