d6284f107b765099621afc746b913c4245b8cda0
Unknown <luke123t@gmail.com>
2018-11-29 15:49:59 -0400

    Add Audio Service Setup

---
+++ LD43/Assets/GameAssets/Scripts/Services/AudioService/AudioAsset.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace DogHouse.Core.Audio
+{
+    /// <summary>
+    /// AudioAsset is a scriptable object that
+    /// will contain serveral parameters that
+    /// describe an audio file that can be
+    /// played by the IAudioService.
+    /// </summary>
+    [CreateAssetMenu(fileName = "MyNewAudioAsset", menuName = "Core/Audio Asset")]
+    public class AudioAsset : ScriptableObject
+    {
+        #region Public Variables
+        public string m_ID;
+        public AudioClip m_audioClip;
+        public bool m_stopOnSceneLoad;
+        public bool m_loop;
+
+        [Range(0,256)]
+        public float m_priority;
+        #endregion
+    }
+}
---
+++ LD43/Assets/GameAssets/Scripts/Services/AudioService/AudioService.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using DogHouse.Core.Services;
+using UnityEngine;
+using DogHouse.Core.Audio;
+using System.Linq;
+
+namespace DogHouse.Services
+{
+    /// <summary>
+    /// AudioService is an implementation of the 
+    /// Audio Service interface. This concrete
+    /// implementation uses several children objects
+    /// each with an audio source to play multiple
+    /// audio files at once.
+    /// </summary>
+    public class AudioService : MonoBehaviour, IAudioService
+    {
+        #region Public Variables
+        #endregion
+
+        #region Private Variables
+        [SerializeField]
+        private int m_numberOfChannels;
+
+        [SerializeField]
+        private AudioAsset[] m_audioAssets;
+
+        private List<AudioSource> m_sources 
+            = new List<AudioSource>();
+        #endregion
+
+        #region Main Methods
+        void OnEnable()
+        {
+            GenerateAudioChannels();
+            RegisterService();
+        }
+
+        void OnDisable()
+        {
+            UnregisterService();
+        }
+
+        public void UnregisterService()
+        {
+            ServiceLocator.Unregister<IAudioService>(this);
+        }
+
+        public void RegisterService()
+        {
+            ServiceLocator.Register<IAudioService>(this);
+        }
+
+        public void Play(string AssetID)
+        {
+            AudioSource source = GetAvailableAudioChannel();
+            AudioAsset asset = FindAudioAsset(AssetID);
+
+            if (source == null || asset == null) return;
+            Play(source, asset);
+        }
+        #endregion
+
+        #region Utility Methods
+        private void GenerateAudioChannels()
+        {
+            for (int i = 0; i < m_numberOfChannels; i++)
+            {
+                CreateAudioChannel();
+            }
+        }
+
+        private void CreateAudioChannel()
+        {
+            GameObject channel = new GameObject();
+            AudioSource source = channel.AddComponent<AudioSource>();
+            source.playOnAwake = false;
+            m_sources.Add(source);
+            channel.transform.parent = transform;
+        }
+
+        private AudioSource GetAvailableAudioChannel()
+        {
+            AudioSource source = m_sources
+                                    .Where(x => x.isPlaying == false)
+                                    .FirstOrDefault();
+
+            return source;
+        }
+
+        private AudioAsset FindAudioAsset(string id)
+        {
+            AudioAsset asset = m_audioAssets
+                                .Where(x => x.m_ID.Equals(id))
+                                .FirstOrDefault();
+
+            return asset;
+        }
+
+        private void Play(AudioSource source, AudioAsset asset)
+        {
+            source.clip = asset.m_audioClip;
+            source.priority = (int)asset.m_priority;
+            source.loop = asset.m_loop;
+            source.Play();
+        }
+        #endregion
+    }
+}
