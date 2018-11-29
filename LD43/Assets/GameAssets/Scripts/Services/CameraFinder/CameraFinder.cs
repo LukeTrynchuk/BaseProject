4e18a960d7213ab01e35b8cfc8b641537add8734
Unknown <luke123t@gmail.com>
2018-11-29 00:58:16 -0400

    Add Camera Finder Service

---
+++ LD43/Assets/GameAssets/Scripts/Services/CameraFinder/CameraFinder.cs
@@ -0,0 +1,68 @@
+﻿using DogHouse.Core.Services;
+using UnityEngine;
+
+namespace DogHouse.Services
+{
+    /// <summary>
+    /// CameraFinder is an implementation of the 
+    /// ICameraFinder interface. The Camera Finder
+    /// service is responsible for keeping track 
+    /// of the Main camera in the scene. 
+    /// </summary>
+    public class CameraFinder : MonoBehaviour, ICameraFinder
+    {
+        #region Public Variables
+        public Camera Camera => GetCamera();
+        #endregion
+
+        #region Private Variables
+        private Camera m_camera;
+        #endregion
+
+        #region Main Methods
+        void OnEnable() => RegisterService();
+        void OnDisable() => UnregisterService();
+
+        public void RegisterService()
+        {
+            ServiceLocator.Register<ICameraFinder>(this);
+        }
+
+        public void UnregisterService()
+        {
+            ServiceLocator.Unregister<ICameraFinder>(this);
+        }
+
+        private Camera GetCamera()
+        {
+            if(m_camera != null) 
+            {
+                return m_camera;
+            }
+
+            m_camera = GetMainCamera();
+            if(m_camera != null)
+            {
+                return m_camera;
+            }
+
+            m_camera = GetAnyCamera();
+            return m_camera;
+        }
+        #endregion
+
+        #region Utility Methods
+        private Camera GetMainCamera()
+        {
+            return Camera.main;
+        }
+
+        private Camera GetAnyCamera()
+        {
+            Camera[] cameras = GameObject.FindObjectsOfType<Camera>();
+            if (cameras.Length > 0) return cameras[0];
+            return null;
+        }
+        #endregion
+    }
+}
---
+++ LD43/Assets/GameAssets/Scripts/Services/CameraFinder/ICameraFinder.cs
@@ -0,0 +1,15 @@
+﻿using DogHouse.Core.Services;
+
+namespace DogHouse.Services
+{
+    /// <summary>
+    /// ICameraFinder is an interface that
+    /// all Camera Finders must implement. A 
+    /// camera finder is responsible for knowing
+    /// the current main camera of the scene.
+    /// </summary>
+    public interface ICameraFinder : IService
+    {
+        UnityEngine.Camera Camera { get; }
+    }
+}
