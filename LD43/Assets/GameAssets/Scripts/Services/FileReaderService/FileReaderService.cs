570e8d2d2a476aa6c043c3d9510ce99c2f6eaccd
Unknown <luke123t@gmail.com>
2018-12-15 01:28:53 -0400

    Add File Reader Service

---
+++ LD43/Assets/GameAssets/Scripts/Services/FileReaderService/FileReaderService.cs
@@ -0,0 +1,22 @@
+﻿using DogHouse.Core.Services;
+using System.IO;
+
+namespace DogHouse.Services
+{
+    /// <summary>
+    /// FileReaderService is a concrete implementation
+    /// of the File Reader Service interface. The 
+    /// File Reeader Service is responsible for reading
+    /// files from the hard drive.
+    /// </summary>
+    public class FileReaderService : BaseService<IFileReaderService>, IFileReaderService
+    {
+        #region Main Methods
+        public string ReadFile(string path)
+        {
+            if (!File.Exists(path)) return default(string);
+            return File.ReadAllText(path);
+        }
+        #endregion
+    }
+}
---
+++ LD43/Assets/GameAssets/Scripts/Services/FileReaderService/IFileReaderService.cs
@@ -0,0 +1,15 @@
+﻿using DogHouse.Core.Services;
+
+namespace DogHouse.Services
+{
+    /// <summary>
+    /// IFileReaderService is an interface that
+    /// all File Reader Services must implement.
+    /// A file reader service is responsible for
+    /// reading files from the hard drive.
+    /// </summary>
+    public interface IFileReaderService : IService
+    {
+        string ReadFile(string path);
+    }
+}
