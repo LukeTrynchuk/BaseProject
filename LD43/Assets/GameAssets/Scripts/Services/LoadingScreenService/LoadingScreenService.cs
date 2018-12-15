6088f960bb3e6cf2b572041ee2792ef947ff255f
Unknown <luke123t@gmail.com>
2018-12-15 12:47:03 -0400

    Setup Loading Screen Service UI

---
+++ LD43/Assets/GameAssets/Scripts/Services/LoadingScreenService/LoadingScreenService.cs
@@ -0,0 +1,30 @@
+﻿using DogHouse.Core.Services;
+
+namespace DogHouse.Services
+{
+    /// <summary>
+    /// LoadingScreenService is an implementation of
+    /// the loading screen service interface. The
+    /// loading screen is responsible for displaying
+    /// a loading screen to the user.
+    /// </summary>
+    public class LoadingScreenService : BaseService<ILoadingScreenService>, ILoadingScreenService
+    {
+        #region Public Variables
+        #endregion
+
+        #region Private Variables
+        private bool m_isLoading = false;
+        #endregion
+
+        #region Main Methods
+        public void SetDisplay(bool value)
+        {
+            m_isLoading = value;
+        }
+        #endregion
+
+        #region Utility Methods
+        #endregion
+    }
+}
